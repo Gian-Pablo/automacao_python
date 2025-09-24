e9a711226fb35ed08ba4a5ef0605aa7f970364d4
Gian Pablo <gian.torres@etec.sp.gov.br>
2025-09-24 13:01:31 -0300

    Interligação de Python com HTML/CSS e Javascript

--- codigo.py
+++ codigo.py
@@ -1,6 +1,20 @@
 import pyautogui
+import time
 
 # Abrir o Google Chrome
 pyautogui.press("win")
 pyautogui.write("Google Chrome")
+pyautogui.press("enter")
+time.sleep(0.5)
+pyautogui.write("http://127.0.0.1:5500/automacao_python/")
+pyautogui.press("enter")
+pyautogui.click(x=1019, y=434)
+pyautogui.write("Gian Pablo Benvive Torres")
+pyautogui.press("tab")
+pyautogui.write("giantorresbenvive@gmail.com")
+pyautogui.press("tab")
+pyautogui.write("147963258@")
+pyautogui.press("tab")
+pyautogui.press("space")
+pyautogui.press("tab")
 pyautogui.press("enter")
---
+++ plano_cartesiano.py
@@ -0,0 +1,6 @@
+import pyautogui
+import time
+
+print(pyautogui.size())
+time.sleep(5)
+print(pyautogui.position())
